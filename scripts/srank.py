d515cffdbdca16bbc451dd8f5b2898a69cb7407e
Matthew Finlayson <matthewf@allenai.org>
2025-06-03 15:44:22 -0400

    min k SVD

--- scripts/srank.py
+++ scripts/srank.py
@@ -1,23 +1,28 @@
-import math
+from math import comb
 import numpy as np
 from jaxtyping import Array, Float, Int, Bool
 
-def covec_of_argsort(argsort: Int[Array, "... n"]) -> Bool[Array, "... math.comb(n,2)"]:
+def covec_of_argsort(argsort: Int[Array, "... n"]) -> Bool[Array, "... comb(n,2)"]:
     """
     Operates on the final dimension
     Returns booleans indicating comparisons directions for each pair.
     """
     comparisons = argsort[..., :, None] > argsort[..., None, :]
-    return comparisons[..., *np.triu_indices(argsort.shape[-1], k=1)]
+    dedup_comparisons = comparisons[..., *np.triu_indices(argsort.shape[-1], k=1)]
+    return dedup_comparisons
 
 rng = np.random.default_rng(0)
 
-n, d = 10, 5
+n, d = 20, 5
 matrix: Float[Array, "n d"] = rng.standard_normal((d, n))
 
-for max_argsort_count in range(1, 100):
+for max_argsort_count in range(1, comb(n, 2) + 5):
     inputs: Float[Array, "m d"] = rng.standard_normal((max_argsort_count, d))
     argsorts: Int[Array, "m n"] = np.unique(np.argsort(inputs @ matrix, axis=-1), axis=0)
-    covecs: Int[Array, "m math.comb(n-1,2)"] = covec_of_argsort(argsorts) * 2 - 1
-    rank = np.linalg.matrix_rank(covecs)
-    assert rank == min(covecs.shape)
+    covecs: Int[Array, "m comb(n,2)"] = covec_of_argsort(argsorts) * 2 - 1
+    U, S, V = np.linalg.svd(covecs, full_matrices=False)
+    np.testing.assert_allclose(U @ np.diag(S) @ V, covecs)
+    k = 1
+    while not (np.sign(U[:, :k] @ V[:k, :]) == covecs).all() and k < min(covecs.shape):
+        k += 1
+    print(k, min(covecs.shape), sep="\t")
