15e55d8e3d1488ee37e2ee18eb8c46c032b04979
Matthew Finlayson <matthewf@allenai.org>
2025-06-03 16:13:02 -0400

    Keeping S in

--- scripts/srank.py
+++ scripts/srank.py
@@ -13,16 +13,17 @@ def covec_of_argsort(argsort: Int[Array, "... n"]) -> Bool[Array, "... comb(n,2)
 
 rng = np.random.default_rng(0)
 
-n, d = 20, 5
+n, d = 10, 5
 matrix: Float[Array, "n d"] = rng.standard_normal((d, n))
 
-for max_argsort_count in range(1, comb(n, 2) + 5):
+# print("Samples", "Rank", sep="\t")
+for max_argsort_count in range(comb(n, 2), comb(n, 2) + 1000):
     inputs: Float[Array, "m d"] = rng.standard_normal((max_argsort_count, d))
     argsorts: Int[Array, "m n"] = np.unique(np.argsort(inputs @ matrix, axis=-1), axis=0)
     covecs: Int[Array, "m comb(n,2)"] = covec_of_argsort(argsorts) * 2 - 1
     U, S, V = np.linalg.svd(covecs, full_matrices=False)
-    np.testing.assert_allclose(U @ np.diag(S) @ V, covecs)
+    np.testing.assert_allclose(np.sign(U @ np.diag(S) @ V), covecs)
     k = 1
-    while not (np.sign(U[:, :k] @ V[:k, :]) == covecs).all() and k < min(covecs.shape):
+    while not (np.sign(U[:, :k] @ np.diag(S[:k]) @ V[:k, :]) == covecs).all() and k < min(covecs.shape):
         k += 1
-    print(k, min(covecs.shape), sep="\t")
+    print(len(argsorts), k, sep="\t")
