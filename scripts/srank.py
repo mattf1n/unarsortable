42f3427167492708badc790f302bbf2c6018b321
Matthew Finlayson <matthewf@allenai.org>
2025-06-06 13:16:37 -0400

    bisect

--- scripts/srank.py
+++ scripts/srank.py
@@ -1,29 +1,90 @@
+import sys
+from bisect import bisect
+from typing import Literal
+from functools import partial
 from math import comb
 import numpy as np
-from jaxtyping import Array, Float, Int, Bool
+import jax, jax.numpy as jnp
+import jaxopt
+from fire import Fire
+from tqdm import tqdm
 
-def covec_of_argsort(argsort: Int[Array, "... n"]) -> Bool[Array, "... comb(n,2)"]:
+type Array = np.ndarray
+
+rng = np.random.default_rng(0)
+
+
+def covec_of_argsort(argsort: Array) -> Array:
     """
     Operates on the final dimension
     Returns booleans indicating comparisons directions for each pair.
     """
-    comparisons = argsort[..., :, None] > argsort[..., None, :]
+    comparisons = argsort[..., :, None] >= argsort[..., None, :]
     dedup_comparisons = comparisons[..., *np.triu_indices(argsort.shape[-1], k=1)]
     return dedup_comparisons
 
-rng = np.random.default_rng(0)
 
-n, d = 10, 5
-matrix: Float[Array, "n d"] = rng.standard_normal((d, n))
+def logistic_loss(params, B):
+    L, R = params
+    p = jax.nn.sigmoid(L @ R)
+    return -jnp.sum(B * jnp.log(p) + (1 - B) * jnp.log(1 - p))
 
-# print("Samples", "Rank", sep="\t")
-for max_argsort_count in range(comb(n, 2), comb(n, 2) + 1000):
-    inputs: Float[Array, "m d"] = rng.standard_normal((max_argsort_count, d))
-    argsorts: Int[Array, "m n"] = np.unique(np.argsort(inputs @ matrix, axis=-1), axis=0)
-    covecs: Int[Array, "m comb(n,2)"] = covec_of_argsort(argsorts) * 2 - 1
-    U, S, V = np.linalg.svd(covecs, full_matrices=False)
-    np.testing.assert_allclose(np.sign(U @ np.diag(S) @ V), covecs)
-    k = 1
-    while not (np.sign(U[:, :k] @ np.diag(S[:k]) @ V[:k, :]) == covecs).all() and k < min(covecs.shape):
-        k += 1
-    print(len(argsorts), k, sep="\t")
+
+def logistic_priciple_component_analysis(B: Array, k: int) -> bool:
+    L = rng.standard_normal((B.shape[0], k)) / jnp.sqrt(k)
+    R = rng.standard_normal((k, B.shape[1])) / jnp.sqrt(k)
+    solver = jaxopt.LBFGS(fun=logistic_loss)
+    (L, R), state = solver.run((L, R), B=B)
+    return bool((jnp.round(jax.nn.sigmoid(L @ R)).astype(int) == B).all())
+
+
+def LP(B, k): 
+    "This one seems harder. How do we get A?"
+
+
+def SVD(U, S, V, B: Array, k: int) -> bool:
+    pred = np.sign(U[:, :k] @ np.diag(S[:k]) @ V[:k, :]).astype(int)
+    return (pred == (B * 2 - 1)).all()
+
+
+def main(
+    n: int,
+    d: int,
+    m: int,
+    method: Literal["svd", "lpca"],
+    start: int = 0,
+    start_type: Literal["rel", "abs"] = "rel",
+    skip: int = 1,
+    interpolation: Literal["linear", "log"] = "linear",
+    num: int | None = None
+):
+    matrix: Array = rng.standard_normal((d, n))
+    if start_type == "rel":
+        start = comb(n, 2) + start
+    stop = comb(n, 2) + m
+    if interpolation == "linear":
+        sample_counts = range(start, stop, skip)
+    elif interpolation == "log":
+        if num is None:
+            raise Exception("num must not be none if interpolation is log")
+        sample_counts = np.logspace(np.log10(start), np.log10(stop), num=num).astype(int)
+    for max_argsort_count in tqdm(sample_counts):
+        inputs: Array = rng.standard_normal((max_argsort_count, d))
+        argsorts: Array = np.unique(
+            np.argsort(inputs @ matrix, axis=-1), axis=0
+        )
+        # print(argsorts.shape, file=sys.stderr)
+        covecs: Array = covec_of_argsort(argsorts)
+        if method == "svd":
+            U, S, V = np.linalg.svd(covecs * 2 - 1, full_matrices=False)
+            np.testing.assert_allclose(np.sign(U @ np.diag(S) @ V), covecs * 2 - 1)
+            is_rank_upper_bound = partial(SVD, U, S, V)
+        elif method == "lpca":
+            is_rank_upper_bound = logistic_priciple_component_analysis
+        key = partial(is_rank_upper_bound, covecs.astype(int))
+        bound = bisect(list(range(comb(n, 2))), 0.5, key=key)
+        print(len(argsorts), bound, sep="\t", flush=True)
+
+
+if __name__ == "__main__":
+    Fire(main)
